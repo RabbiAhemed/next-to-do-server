fe1cd8af7105d53de3dcee874d6e3858d92900c2
Rabbi Ahmed Limon <rabbiahemed>
2022-12-29 16:44:12 +0600

    delete task

--- index.js
+++ index.js
@@ -21,19 +21,28 @@ async function run() {
   try {
     const tasksCollection = client.db("NextToDo").collection("tasks");
 
+    //Get All Tasks Data
+    app.get("/tasks", async (req, res) => {
+      const query = {};
+      const cursor = tasksCollection.find(query);
+      const tasks = await cursor.toArray();
+      res.send(tasks);
+    });
     // Add Task
     app.post("/tasks", async (req, res) => {
       const newTaskInfo = req.body;
       const newTask = await tasksCollection.insertOne(newTaskInfo);
       res.send(newTask);
     });
-
-    // All Tasks Data
-    app.get("/tasks", async (req, res) => {
-      const query = {};
-      const cursor = tasksCollection.find(query);
-      const tasks = await cursor.toArray();
-      res.send(tasks);
+    // Delete Task
+    app.delete("/tasks/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(req.params);
+      const query = { _id: ObjectId(id) };
+      const result = await tasksCollection.deleteOne(query);
+      res.send(result);
+      console.log("deleting", id);
+      console.log(result);
     });
   } finally {
   }
